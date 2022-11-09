d3a5cbb4f11da21c9c1a83107e1aaf26364c8a97
David <santiago.09dsmr@gmail.com>
2022-11-09 16:38:07 -0500

    consol

---
+++ Udemy/consol.py
@@ -0,0 +1,13 @@
+my_range = range(1,5)
+# for i in my_range:
+#     print(i)
+
+my_range = range(0,7,2)
+my_other_range = range(0,8,2)
+my_range == my_other_range
+# for i in my_range:
+#     print(i)
+# for i in my_other_range:
+#     print(i)
+# for i in range(0,101,2):
+#     print(i)
