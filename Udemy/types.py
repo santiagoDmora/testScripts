bf8a8720ed795c4c2d031516df8318e865d9886a
David Mora <santiago.09dsmr@gmail.com>
2022-11-12 13:31:30 -0500

    types.py

---
+++ Udemy/types.py
@@ -0,0 +1,20 @@
+#String
+my_name = 'David'
+my_name = "Santiago"
+print(my_name)
+print(type(my_name))
+
+#int
+my_age = 23
+print(my_age)
+print(type(my_age))
+
+#Boolean
+is_single = True
+is_single = False
+print(type(is_single))
+
+#inputs
+my_age = input("Cual es tu edad? ")
+print("my age es: " , my_age)
+print(type(my_age))
---
+++ Udemy/vars.py
@@ -0,0 +1,14 @@
+
+#Variable nombre
+var1 = "David"
+print(var1)
+#VARIABLE NUMERO
+my_age = 23
+print(my_age)
+
+var1 = 'Santiago'
+print(var1)
+
+#input
+my_name = input("Cual es tu nombre?: ")
+print("tu nombre es" , my_name)
