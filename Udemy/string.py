ff332935a2176531a581744d565b69167acff078
David Mora <santiago.09dsmr@gmail.com>
2022-11-12 15:22:36 -0500

    string.py

---
+++ Udemy/string.py
@@ -0,0 +1,26 @@
+name = "Santiago"
+last_name = "Mora"
+print(name)
+print(last_name)
+
+
+#concatenacion 
+full_name = name + ' ' + last_name
+print(full_name)
+
+#Uso de comillas
+quote = "I'm Nicolas"
+print(quote)
+
+quote2 = "She said 'hello'"
+print(quote2)
+
+#format
+template = "Hola mi nombre es " + name + " y mi apellido es " + last_name
+print(template)
+
+template2 = "Hola , mi nombre es {} y mi apellido es {}".format(name,last_name)
+print(template2)
+
+template = f"hola, mi nombre es {name} y mi apellido es {last_name} "
+print(template)
