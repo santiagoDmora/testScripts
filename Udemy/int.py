d1d90352da2b9842d240ac8c75ce0d64049ded4c
David Mora <santiago.09dsmr@gmail.com>
2022-11-12 15:47:25 -0500

    booleans.py

---
+++ Udemy/int.py
@@ -0,0 +1,31 @@
+lives = 3
+print(type(lives))
+age = 12
+buget = 100
+
+#float
+temperatura = 12.12
+print(type(temperatura))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 455555555555555555555555555555555555.1
+print(number)
+
+number = 0.000000000000000000000000000001
+print(number)
