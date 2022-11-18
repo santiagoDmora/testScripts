ef3b7755d0d69472ef9f8a459f60880164cd8939
David <santiago.09dsmr@gmail.com>
2022-11-18 16:49:36 -0500

    transformation.py

---
+++ Udemy/transformation.py
@@ -0,0 +1,19 @@
+name = "David"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("David" + "Mora")
+print(10 + 20)
+
+age = 22
+print("Mi edad es " + str(age))
+print (F" Mi edad es {age}")
+
+age = input("Escribe tu edad: ")
+print(type(age))
+age = int(age)
+age += 10
+print (F"Tu edad en 10 años sera: {age} ")
